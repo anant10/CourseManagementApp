2ef19d41eeb04ab4a98ffb850cb988d20fbed409
anant10 <anantshanbhag93@gmail.com>
2020-05-09 15:36:46 -0400

    Initial commit

---
+++ src/main/java/com/webdev/staticui/StaticuiApplication.java
@@ -0,0 +1,13 @@
+package com.webdev.staticui;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StaticuiApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StaticuiApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/webdev/staticui/StaticuiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.webdev.staticui;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StaticuiApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
